d56e269133812ba290775ecb594c5958aa401120
Ilya Abrashkin <iliacmd@yandex.ru>
2018-09-05 11:40:44 +0300

    Add models Book, Author

---
+++ models/Author.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "author".
+ *
+ * @property int $id
+ * @property string $firstName
+ * @property string $lastName
+ *
+ * @property BookAuthor[] $bookAuthors
+ * @property Book[] $books
+ */
+class Author extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'author';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['firstName'], 'required'],
+            [['firstName', 'lastName'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstName' => 'First Name',
+            'lastName' => 'Last Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBookAuthors()
+    {
+        return $this->hasMany(BookAuthor::class, ['author_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Book::class, ['id' => 'book_id'])->viaTable('book_author', ['author_id' => 'id']);
+    }
+}
---
+++ models/Book.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ * @property string $title
+ *
+ * @property BookAuthor[] $bookAuthors
+ * @property Author[] $authors
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBookAuthors()
+    {
+        return $this->hasMany(BookAuthor::class, ['book_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthors()
+    {
+        return $this->hasMany(Author::class, ['id' => 'author_id'])->viaTable('book_author', ['book_id' => 'id']);
+    }
+}
